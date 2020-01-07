9888bc765287446bbf1cf0e02980369188866a2c
rsninja722 <jnick722547@gmail.com>
2020-01-07 16:41:03 -0500

    2020 added

--- fieldLinks.js
+++ fieldLinks.js
@@ -1,6 +1,11 @@
 // to add a field, put another object in the array with the desired URLs
 // the first field in the array will be selected by default
 var links = [
+    // 2020
+    {
+        imageURL:"resources/2020-Field.png",
+        jsonURL:"resources/2020-InfiniteRecharge.json"
+    },
     // 2019
     {
         imageURL: "https://raw.githubusercontent.com/wpilibsuite/PathWeaver/master/src/main/resources/edu/wpi/first/pathweaver/2019-field.jpg",
